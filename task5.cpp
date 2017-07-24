a70338ac86aff18f9333b80c73b15f11907372a4
radko <justslavic@gmail.com>
2017-07-24 14:17:06 +0300

    replace int to double in point, add contains_in_rectangle() to filter cross points

--- task5.cpp
+++ task5.cpp
@@ -5,25 +5,41 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <cmath>
 
 using namespace std;
 
 struct point {
-    int x = 0;
-    int y = 0;
+    double x = 0;
+    double y = 0;
 
     point () {}
-    point (const int& _x, const int& _y) :x(_x), y(_y) {}
+    point (const double& _x, const double& _y) :x(_x), y(_y) {}
 };
 
 struct line {
     point p1, p2;
 
-    line(const int& _x1, const int& _y1, const int& _x2, const int& _y2) {
+    line(const double& _x1, const double& _y1, const double& _x2, const double& _y2) {
         p1 = point(_x1, _y1);
         p2 = point(_x2, _y2);
     }
     line(const point& _p1, const point& _p2) :p1(_p1), p2(_p2) {}
+    bool contains_in_rectangle(const point& p) const {
+        bool res = true;
+        if (p1.x > p2.x) {
+            res = res && p.x >= p2.x && p.x <= p1.x;
+        } else {
+            res = res && p.x >= p1.x && p.x <= p2.x;
+        }
+        if (p1.y > p2.y) {
+            res = res && p.y >= p2.y && p.y <= p1.y;
+        } else {
+            res = res && p.y >= p1.y && p.y <= p2.y;
+        }
+
+        return res;
+    }
 };
 
 ostream& operator<<(ostream& os, const point& p) {
@@ -59,20 +75,23 @@ void build_graph(const vector<line>& lines) {
 
     for (int i = 0; i < lines.size(); ++i) {
         for (int j = i+1; j < lines.size(); ++j) {
-            const int& x1 = lines[i].p1.x; const int& y1 = lines[i].p1.y;
-            const int& x2 = lines[i].p2.x; const int& y2 = lines[i].p2.y;
-            const int& x3 = lines[j].p1.x; const int& y3 = lines[j].p1.y;
-            const int& x4 = lines[j].p2.x; const int& y4 = lines[j].p2.y;
+            const double& x1 = lines[i].p1.x; const double& y1 = lines[i].p1.y;
+            const double& x2 = lines[i].p2.x; const double& y2 = lines[i].p2.y;
+            const double& x3 = lines[j].p1.x; const double& y3 = lines[j].p1.y;
+            const double& x4 = lines[j].p2.x; const double& y4 = lines[j].p2.y;
 
-            int divider = (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4);
+            double divider = (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4);
 
-            if (abs(divider) < eps) {
+            if (fabs(divider) < eps) {
                 cout << "lines " << lines[i] << " and " << lines[j] << " is parallel" << endl;
             } else {
                 cross_x = ((x1*y2 - y1*x2)*(x3 - x4) - (x1 - x2)*(x3*y4 - y3*x4)) / divider;
                 cross_y = ((x1*y2 - y1*x2)*(y3 - y4) - (y1 - y2)*(x3*y4 - y3*x4)) / divider;
 
-                cout << "cross = (" << cross_x << ", " << cross_y << ")" << endl;
+                if (lines[i].contains_in_rectangle(point(cross_x, cross_y)) &&
+                        lines[j].contains_in_rectangle(point(cross_x, cross_y))) {
+                    cout << "cross = (" << cross_x << ", " << cross_y << ")" << endl;
+                }
             }
         }
     }
@@ -92,6 +111,7 @@ int main() {
     cout << "home1 = " << home1 << endl;
     cout << "home2 = " << home2 << endl;
 
+    cout << endl;
     build_graph(roads);
 
     return 0;
