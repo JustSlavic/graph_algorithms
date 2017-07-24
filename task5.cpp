2bf3a05d7db2792632029d48ad7b3e694e45e262
radko <justslavic@gmail.com>
2017-07-24 12:17:09 +0300

    add structs for point and line and read_data()

--- task5.cpp
+++ task5.cpp
@@ -6,6 +6,41 @@
 
 using namespace std;
 
+struct point {
+    int x, y;
+
+    point (const int& _x, const int& _y) :x(_x), y(_y) {}
+};
+
+struct line {
+    point p1, p2;
+
+    line(const int& _x1, const int& _y1, const int& _x2, const int& _y2) {
+        p1 = point(_x1, _y1);
+        p2 = point(_x2, _y2);
+    }
+    line(const point& _p1, const point& _p2) :p1(_p1), p2(_p2) {}
+};
+
+void read_data(const string& filename, vector<line>& lines, point& home1, point& home2) {
+    ifstream ifs(filename);
+
+    unsigned N;
+    ifs >> N;
+    lines.reserve(N);
+
+    for (int i = 0; i < N; ++i) {
+        int tmp1, tmp2, tmp3, tmp4;
+        ifs >> tmp1 >> tmp2 >> tmp3 >> tmp4;
+
+        lines.emplace_back(line(tmp1, tmp2, tmp3, tmp4));
+    }
+
+    ifs >> home1.x >> home1.y >> home2.x >> home2.y;
+
+    ifs.close();
+}
+
 int main() {
 
     cout << "Hello, World!" << endl;
